e006e431320f6ec2b2592a984c4b2526ee38d3db
k22141yamada <154310911+k22141yamada@users.noreply.github.com>
2023-12-21 15:22:48 +0900

    輪郭部分の処理

--- gazousyori/rinkaku.py
+++ gazousyori/rinkaku.py
@@ -1,4 +1,12 @@
 import cv2
-import numpy as np
 
-filePath = '.png'
+# グレースケールで画像を読み込む
+src_img = cv2.imread("###.jpg", cv2.IMREAD_GRAYSCALE) #名前をもらいたい
+# Canny edge detection
+canny_img = cv2.Canny(src_img, 50, 300)
+
+# 入力画像とCanny edge detectionの処理画像の表示
+cv2.imshow("Image", src_img)
+cv2.imshow("Canny", canny_img)
+cv2.imwrite('canny.jpg',canny_img,[cv2.IMWRITE_JPEG_QUALITY, 50])#ここら辺どう繋げるか
+cv2.waitKey()
