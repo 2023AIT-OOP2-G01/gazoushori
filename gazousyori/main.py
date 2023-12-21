6510f98cb5507b9a77bc8a4a44dc7fdbadfe9ac8
aren <k22033kk@aitech.ac.jp>
2023-12-21 15:42:17 +0900

    Web更新

--- gazousyori/main.py
+++ gazousyori/main.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    input = request.form.get('name')
+
+
     return render_template('index.html')
+
+@app.route('/gazousyori1')
+def gazousyori1():
+    return render_template('gazousyori1.html')
+
+@app.route('/gazousyori2')
+def gazousyori2():
+    return render_template('gazousyori2.html')
+
+@app.route('/gazousyori3')
+def gazousyori3():
+    return render_template('gazousyori3.html')
 
 
 if __name__ == '__main__':
